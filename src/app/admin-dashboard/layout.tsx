027c8469b18771dab587dffb91f6ee2296466690
Kongkon-79 <kongkonjowarder@gmail.com>
2025-05-08 14:05:40 +0600

    auth problems solve

--- src/app/admin-dashboard/layout.tsx
+++ src/app/admin-dashboard/layout.tsx
@@ -1,16 +1,19 @@
 import React, { ReactNode } from "react";
 import DashboardSidebar from "./_components/dashboard-sidebar";
 import DashboardNavbar from "./_components/dashboard-navbar";
+import AuthWrapper from "@/components/provider/AuthWrapper";
 
 const AdminDashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-white w-full ">
-      <DashboardNavbar />
-      <div className="w-full h-full flex justify-start items-start ">
-        <DashboardSidebar />
-        <div className="w-full pl-7 pr-8 pt-6">{children}</div>
+    <AuthWrapper>
+      <div className="bg-white w-full ">
+        <DashboardNavbar />
+        <div className="w-full h-full flex justify-start items-start ">
+          <DashboardSidebar />
+          <div className="w-full pl-7 pr-8 pt-6">{children}</div>
+        </div>
       </div>
-    </div>
+    </AuthWrapper>
   );
 };
 
